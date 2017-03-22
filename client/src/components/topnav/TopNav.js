0d56a5650e5f73bb45e6566ca82ba52af7a655e6
Alex Treppass <alextreppass@gmail.com>
2017-03-22 18:01:10 +0000

    Update backpack mixins, stylesheets, node-sass. Add logo svg

--- client/src/components/topnav/TopNav.js
+++ client/src/components/topnav/TopNav.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './TopNav.scss';
+import logo from '../..//logo.svg';
 
 const TopNav = () => (
   <header className='header'>
     <a href="/">
       <span className='logoText'>Skyscanner</span>
-      <span className='logo' />
+      <img className='logo' alt="Skyscanner" src={logo}/>
     </a>
      // TODO burger menu icon
   </header>
