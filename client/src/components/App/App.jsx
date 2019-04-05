0dca61b52f9da8a12bf1b4dcf10c208edbdc10aa
George Gillams <george.gillams@skyscanner.net>
2019-04-05 16:25:23 +0100

    Rename getClassName

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -4,12 +4,12 @@ import BpkText from 'bpk-component-text';
 import STYLES from './App.scss';
 import Header from './../Header';
 
-const c = className => STYLES[className] || 'UNKNOWN';
+const getClassName = className => STYLES[className] || 'UNKNOWN';
 
 const App = () => (
-  <div className={c('App')}>
+  <div className={getClassName('App')}>
     <Header />
-    <main className={c('App__main')}>
+    <main className={getClassName('App__main')}>
       <BpkText tagName="p">Over to you...</BpkText>
     </main>
   </div>
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import logo from './logo.svg';
 import STYLES from './Header.scss';
 
-const c = className => STYLES[className] || 'UNKNOWN';
+const getClassName = className => STYLES[className] || 'UNKNOWN';
 
 const Header = () => (
-  <header className={c('Header')}>
+  <header className={getClassName('Header')}>
     <a href="/">
-      <span className={c('Header__hidden-text')}>Skyscanner</span>
-      <img className={c('Header__logo-image')} alt="Skyscanner" src={logo} />
+      <span className={getClassName('Header__hidden-text')}>Skyscanner</span>
+      <img className={getClassName('Header__logo-image')} alt="Skyscanner" src={logo} />
     </a>
     {/* TODO burger menu icon */}
   </header>
