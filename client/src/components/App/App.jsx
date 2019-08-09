23f83749c5230166ca34fca7a66fbdb365178344
Ollie Curtis <oliver.curtis@skyscanner.net>
2019-08-09 11:24:44 +0100

    ESLint fixes

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import BpkText from "bpk-component-text";
+import React from 'react';
+import BpkText from 'bpk-component-text';
 
-import STYLES from "./App.scss";
-import Header from "./../Header";
+import STYLES from './App.scss';
+import Header from './../Header';
 
-const getClassName = className => STYLES[className] || "UNKNOWN";
+const getClassName = className => STYLES[className] || 'UNKNOWN';
 
 const App = () => (
-  <div className={getClassName("App")}>
+  <div className={getClassName('App')}>
     <Header />
-    <main className={getClassName("App__main")}>
+    <main className={getClassName('App__main')}>
       <BpkText tagName="p">Over to you...</BpkText>
       {/* TODO: Add an information blue header component here */}
 
