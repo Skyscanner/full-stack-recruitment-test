edc45e924b2e7a6f537b75e95e3f0ba777926cad
Ollie Curtis <oliver.curtis@skyscanner.net>
2019-08-09 11:15:38 +0100

    Added a simple server test for FS test

---
+++ server/src/server.test.js
@@ -0,0 +1,13 @@
+test('a simple starter test', async (done) => {
+  const finished = [];
+  const testFun = (x1, x2) => {
+    finished.push(x1 + x2);
+  };
+
+  await testFun(1, 2);
+  await testFun(2, 3);
+  await testFun(3, 4);
+
+  expect(finished).toEqual([3, 5, 7]);
+  done();
+});
