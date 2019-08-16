f3e1fff874599b3a00c32c28b1ca4cd01253bc31
Ollie Curtis <8831547+ojcurtis@users.noreply.github.com>
2019-08-16 14:21:07 +0100

    Update client/src/index.js
    Co-Authored-By: Matthew Davidson <matthewdavidson@users.noreply.github.com>

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import App from './components/App';
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
 
-/* Create fuctionality in a sensible file to od the following:
+/* Create functionality in a sensible file to do the following:
  * TODO - retrieve the results from the provided flight.json
  * TODO - a better format for displaying results to the results page.
  */
