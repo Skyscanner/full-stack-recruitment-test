8f3b61aa8ddfed5524d0e24b2c95ac9c91017dda
Ollie Curtis <8831547+olliecurtis@users.noreply.github.com>
2020-10-07 12:56:39 +0100

    Updating dependencies to latest

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-/* eslint-disable no-console */
-// Disabling 'no-console' as it's reasonable for this file to do some logging.
-
 import 'bpk-stylesheets';
+import 'bpk-stylesheets/font';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
